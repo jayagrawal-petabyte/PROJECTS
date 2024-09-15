5304b61e91bfedcf1f85548873bf227a760fc653
JAY AGRAWAL <jayagrawal72511@gmail.com>
2024-09-15 17:34:38 +0530

    Update CALCULATOR.c

--- CALCULATOR.c
+++ CALCULATOR.c
@@ -1,1 +1,39 @@
 
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    char op;
+    printf("Enter 2 numbers to operate on");
+    scanf("%d %d", &a, &b);
+
+    printf("Enter the operator");
+    scanf("%c",&op);
+  for(;;)
+  {
+    switch(op)
+    {
+        case '+':
+        printf("HENCE THE CALCULATION IS\n%d + %d = %d",a,b,a+b);
+        break;
+
+        case '-':
+        printf("HENCE THE CALCULATION IS\n%d - %d = %d",a,b,a-b);
+        break;
+
+        case '*':
+        printf("HENCE THE CALCULATION IS\n%d * %d = %d",a,b,a*b);
+        break;
+
+        case '/':
+        printf("HENCE THE CALCULATION IS\n%d / %d = %d",a,b,a/b);
+        break;
+
+        default:
+        printf("Enter valid operator");
+        break;
+    }
+  }
+  return 0;
+}
